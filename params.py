c1e44b8cb0346f7c55ff2ac2673f890021c5a24a
Brendan Guille <brendan.guille@gmail.com>
2025-03-18 09:59:07 +0100

    bren packages

--- params.py
+++ params.py
@@ -7,15 +7,18 @@ BUCKET_NAME = os.environ.get('BUCKET_NAME')
 MODEL_TARGET = os.environ.get('MODEL_TARGET')
 BUCKET_MODEL = os.environ.get('BUCKET_MODEL')
 
+####### Déterminer le chemin de base du projet (dossier contenant ce script)####
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
 ##################  CHEMINS DES DONNÉES  ##################
-ML_DATA_PATH = "/home/bren/code/ThomasLechanoine/Breast_Cancer_Project/Data/Machine_learning.csv"
-DL_ZIP_PATH = "Data/Data_prepros.zip"
-DL_DATA_PATH = "Data/Data_Deep_Learning"
+ML_DATA_PATH = os.path.join(BASE_DIR, "Data", "Machine_learning.csv")
+DL_ZIP_PATH = os.path.join(BASE_DIR, "Data", "Data_prepros.zip")
+DL_DATA_PATH = os.path.join(BASE_DIR, "Data", "Data_Deep_Learning")
 
 ##################  CHEMINS DES MODÈLES  ##################
-DL_MODEL_PATH = "Deep_learning/models_saved/best_model.h5"
-ML_MODEL_PATH = "/home/bren/code/ThomasLechanoine/Breast_Cancer_Project/Machine_learning/models_saved/ml_best_model.pkl"
-ML_SCALER_PATH = "/home/bren/code/ThomasLechanoine/Breast_Cancer_Project/Machine_learning/models_saved/ml_scaler.pkl"
+DL_MODEL_PATH = os.path.join(BASE_DIR, "Deep_learning", "models_saved", "best_model.h5")
+ML_MODEL_PATH = os.path.join(BASE_DIR, "Machine_learning", "models_saved", "ml_best_model.pkl")
+ML_SCALER_PATH = os.path.join(BASE_DIR, "Machine_learning", "models_saved", "ml_scaler.pkl")
 
 ##################  PARAMÈTRES D'ENTRAÎNEMENT  ##################
 DL_BATCH_SIZE = 32
